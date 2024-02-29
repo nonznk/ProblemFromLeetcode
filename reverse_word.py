300feb0ad3acbf69f4c07b7085d75824071ed68d
Kanon Saetang <kanon_1042@hotmail.com>
2024-02-29 19:16:43 +0700

    Completed main function

--- reverse_word.py
+++ reverse_word.py
@@ -1,9 +1,21 @@
-s = input("Input: ")
+txt = input("Input: ")
 
 new_string = ""
 
-for i in range(len(s)):
-    if s[i] != '"':
-        new_string = new_string + s[i]
+for i in range(len(txt)):
+    if txt[i] != '"':
+        new_string = new_string + txt[i]
 
-print(new_string)
+text_sep = new_string.split(" ")
+# print(text_sep)
+
+new_text = ""
+
+for j in range(len(text_sep)):
+    for k in range(len(text_sep[j]) - 1, -1, -1):
+        new_text = new_text + text_sep[j][k]
+    new_text = new_text + " "
+
+new_text = '"' + new_text + '"'
+
+print(new_text)
