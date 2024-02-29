2c72f2361dac8c8c0f613d10290445cfdb9507cf
Kanon Saetang <kanon_1042@hotmail.com>
2024-02-29 20:11:05 +0700

    Modify completed

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,29 @@
 s = input("")
 
+# new_string = ""
+
+# for i in range(len(s)):
+#     if s[i] != '"':
+#         new_string = new_string + s[i]
+
+# text_sep = new_string.split(" ")
+# # print(text_sep)
+
+# new_text = ""
+
+# for j in range(len(text_sep)):
+#     num = len(text_sep)
+#     for k in range(len(text_sep[j]) - 1, -1, -1):
+#         new_text = new_text + text_sep[j][k]
+
+#     if j < num - 1:
+#         new_text = new_text + " "
+
+# new_text = '"' + new_text + '"'
+
+# # print("Output: {}".format(new_text))
+# print(new_text)
+
 
 class Solution:
     # def __init__(self, s):
@@ -26,7 +50,9 @@ class Solution:
                 self.new_text = self.new_text + " "
 
         self.new_text = '"' + self.new_text + '"'
-        return self.new_text
+
+        # print("Output: {}".format(new_text))
+        print(self.new_text)
 
     def __str__(self):
         return self.new_text
@@ -34,4 +60,4 @@ class Solution:
 
 a = Solution()
 a.reverseWords(s)
-print(a)
+# print(a)
