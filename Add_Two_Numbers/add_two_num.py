d0ee726c2401e4273fc98969533488df8fd0e41b
Kanon Saetang <kanon_1042@hotmail.com>
2024-03-04 20:55:30 +0700

    Completed problem with null bug

--- Add_Two_Numbers/add_two_num.py
+++ Add_Two_Numbers/add_two_num.py
@@ -3,20 +3,29 @@ class Solution:
         self.l1 = l1
         self.l2 = l2
         result = []
+        new_l1 = ""
+        new_l2 = ""
 
         self.l1.reverse()
         self.l2.reverse()
-        
-        for i, j in zip(self.l1, self.l2):
-            result.append(i + j) # [7, 10, 7]
 
-        for k in range(len(result)):
-            if result[k] >= 10:
-                result[k] = result[k] - 10
-                result[k-1] = result[k-1] + 1
-        
+        for i in range(len(self.l1)):
+            new_l1 = new_l1 + str(self.l1[i])
+        for j in range(len(self.l2)):
+            new_l2 = new_l2 + str(self.l2[j])
+
+        new_result = int(new_l1) + int(new_l2)
+        new_result_str = str(new_result)
+
+        for k in range(len(new_result_str)):
+            result_toint = new_result_str[k]
+            result.append(int(result_toint))
+
         result.reverse()
-
+        return result
         # print(result)
 
+
 # a = Solution().addtwoNumbers([2, 4, 3], [5, 6, 4])
+# a = Solution().addtwoNumbers([0], [0])
+# a = Solution().addtwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9])
